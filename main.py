a2aff287b304b1a47fe9dfa4d77c886357ba789b
snevr <s.nevrovsky@ya.ru>
2023-05-24 20:33:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect
+from projects import projects
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', projects=projects)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80)
