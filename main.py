7493212446edd2b56ca0219db0dd39b5c68b30db
snevr <s.nevrovsky@ya.ru>
2023-06-27 17:58:16 +0300

    add function to add buttons

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, send_file
 from projects import projects
 
 app = Flask(__name__)
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', projects=projects)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
--- projects.py
+++ projects.py
@@ -1,17 +1,23 @@
 # Массив словарей с данными о проектах
+buttons = [
+    {'name': 'Кнопка 3', 'cl': 'success', 'link': 'http://example.com/button3'}
+]
+
 projects = [
     {
         "name": "Телеграм бот собираюший идеи для улучшения школы",
         "des": "В этого бота можно написать свою идею, смотреть и лайкать другие идеи. Бот написан на Python с использованием PyTelegramBotAPI и базы даных SQLite",
         "link": "https://t.me/school_1561_bot",
         "image": "static/project_foto/bot_for_ideas.png",
-        "git": "https://github.com/NRF24l01/bot_for_ideas"
+        "git": "https://github.com/NRF24l01/bot_for_ideas",
+        "but": buttons
     },
     {
         "name": "Сайт с заметками",
         "des": """Сайт в котором каждый может создать свои заметки, редактировать их, помечать выполнеными и удалять. Написан на Python с помощью Flask и базы даных SQLite""",
         "link": "",
         "image": "static/project_foto/zametki_site.png",
-        "git": "https://github.com/NRF24l01/zametki_site"
+        "git": "https://github.com/NRF24l01/zametki_site",
+        "but": buttons
     }
 ]
